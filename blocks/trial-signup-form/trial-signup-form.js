8876b22791bc2335ed21f6eec38ebe62854fe6ea
Bruce Lefebvre <blefebvr@adobe.com>
2023-05-16 14:42:52 -0400

    Read signup form link from the block config

--- blocks/trial-signup-form/trial-signup-form.js
+++ blocks/trial-signup-form/trial-signup-form.js
@@ -5,8 +5,8 @@ export default async function decorate(block) {
   block.textContent = '';
 
   const iframe = document.createElement('iframe');
-  iframe.src = 'https://commerce.adobe.com/business-trial/sign-up?items%5B0%5D%5Bid%5D=649A1AF5CBC5467A25E84F2561274821&cli=tester&co=US&lang=en';
-  iframe.height = cfg.height || '315';
+  iframe.src = cfg['form-link'];
+  iframe.height = cfg.height || '800';
   block.appendChild(iframe);
   block.classList.add('visible');
 }
