867769251c8fad0eb0b3791bd2a20da52b55af02
Sweta Barman <swetabarman@gmail.com>
2023-07-18 10:23:07 -0400

    Add comments to the functions + remove unused code

--- blocks/form/form.js
+++ blocks/form/form.js
@@ -1,37 +1,18 @@
-function createSelect(fd) {
-  const select = document.createElement('select');
-  select.id = fd.Field;
-  if (fd.Placeholder) {
-    const ph = document.createElement('option');
-    ph.textContent = fd.Placeholder;
-    ph.setAttribute('selected', '');
-    ph.setAttribute('disabled', '');
-    select.append(ph);
-  }
-  fd.Options.split(',').forEach((o) => {
-    const option = document.createElement('option');
-    option.textContent = o.trim();
-    option.value = o.trim();
-    select.append(option);
-  });
-  if (fd.Mandatory === 'x') {
-    select.setAttribute('required', 'required');
-  }
-  return select;
-}
-
 function constructPayload(form) {
   const payload = {};
   [...form.elements].forEach((fe) => {
-    if (fe.type === 'checkbox') {
-      if (fe.checked) payload[fe.id] = fe.value;
-    } else if (fe.id) {
+    if (fe.id) {
       payload[fe.id] = fe.value;
     }
   });
   return payload;
 }
 
+/**
+ * Once the payload is constructed and the form entry is validated,
+ * we submit the form. It creates an entry in the spreadsheet to store the form
+ * data.
+ */
 async function submitForm(form) {
   const payload = constructPayload(form);
 
@@ -47,6 +28,10 @@ async function submitForm(form) {
   return payload;
 }
 
+/**
+ * We check the validity of the email entry. We only accept company email addresses
+ * as of now.
+ */
 function checkValidity(form) {
   const payload = constructPayload(form);
   const { email } = payload;
@@ -54,20 +39,24 @@ function checkValidity(form) {
   return String(email).match(regex);
 }
 
-function createButton(fd) {
+/**
+ * We create a submit button for the form.
+ * Once we click on submit, if the email entry is valid, we disable
+ * the submit button and redirect the user to the thank you page.
+ */
+function createButton(field) {
   const button = document.createElement('button');
-  button.textContent = fd.Label;
+  button.textContent = field.Label;
   button.classList.add('button');
   button.classList.add('primary');
-  if (fd.Type === 'submit') {
+  if (field.Type === 'submit') {
     button.addEventListener('click', async (event) => {
       const form = button.closest('form');
-      if (fd.Placeholder) form.dataset.action = fd.Placeholder;
-      if (form.checkValidity() && checkValidity(form) !== null) {
+      if (checkValidity(form) !== null) {
         event.preventDefault();
         button.setAttribute('disabled', '');
         await submitForm(form);
-        const redirectTo = fd.Extra;
+        const redirectTo = field.Extra;
         window.location.href = redirectTo;
       } else {
         event.preventDefault();
@@ -87,60 +76,19 @@ function createButton(fd) {
   return button;
 }
 
-function createHeading(fd) {
-  const heading = document.createElement('h3');
-  heading.textContent = fd.Label;
-  return heading;
-}
-
-function createInput(fd) {
+/**
+ * We create an input field and pick up the type, id, and placeholder text
+ * from the helix-default spreadsheet on sharepoint.
+ */
+function createInput(field) {
   const input = document.createElement('input');
-  input.type = fd.Type;
-  input.id = fd.Field;
-  input.setAttribute('placeholder', fd.Placeholder);
-  if (fd.Mandatory === 'x') {
+  input.type = field.Type;
+  input.id = field.Field;
+  input.setAttribute('placeholder', field.Placeholder);
+  if (field.Mandatory === 'x') {
     input.setAttribute('required', 'required');
   }
   return input;
-}
-
-function createTextArea(fd) {
-  const input = document.createElement('textarea');
-  input.id = fd.Field;
-  input.setAttribute('placeholder', fd.Placeholder);
-  if (fd.Mandatory === 'x') {
-    input.setAttribute('required', 'required');
-  }
-  return input;
-}
-
-function createLabel(fd) {
-  const label = document.createElement('label');
-  label.setAttribute('for', fd.Field);
-  label.textContent = fd.Label;
-  if (fd.Mandatory === 'x') {
-    label.classList.add('required');
-  }
-  return label;
-}
-
-function applyRules(form, rules) {
-  const payload = constructPayload(form);
-  rules.forEach((field) => {
-    const {
-      type,
-      condition: { key, operator, value },
-    } = field.rule;
-    if (type === 'visible') {
-      if (operator === 'eq') {
-        if (payload[key] === value) {
-          form.querySelector(`.${field.fieldId}`).classList.remove('hidden');
-        } else {
-          form.querySelector(`.${field.fieldId}`).classList.add('hidden');
-        }
-      }
-    }
-  });
 }
 
 function fill(form) {
@@ -152,59 +100,33 @@ function fill(form) {
   }
 }
 
+/**
+ * We create the form and add the submit button and input text field to it.
+ */
 async function createForm(formURL) {
   const { pathname } = new URL(formURL);
   const resp = await fetch(pathname);
   const json = await resp.json();
   const form = document.createElement('form');
-  const rules = [];
   // eslint-disable-next-line prefer-destructuring
   form.dataset.action = pathname.split('.json')[0];
-  json.data.forEach((fd) => {
-    fd.Type = fd.Type || 'text';
+  json.data.forEach((field) => {
+    field.Type = field.Type || 'text';
     const fieldWrapper = document.createElement('div');
-    const style = fd.Style ? ` form-${fd.Style}` : '';
-    const fieldId = `form-${fd.Type}-wrapper${style}`;
+    const style = field.Style ? ` form-${field.Style}` : '';
+    const fieldId = `form-${field.Type}-wrapper${style}`;
     fieldWrapper.className = fieldId;
     fieldWrapper.classList.add('field-wrapper');
-    switch (fd.Type) {
-      case 'select':
-        fieldWrapper.append(createLabel(fd));
-        fieldWrapper.append(createSelect(fd));
-        break;
-      case 'heading':
-        fieldWrapper.append(createHeading(fd));
-        break;
-      case 'checkbox':
-        fieldWrapper.append(createInput(fd));
-        fieldWrapper.append(createLabel(fd));
-        break;
-      case 'text-area':
-        fieldWrapper.append(createLabel(fd));
-        fieldWrapper.append(createTextArea(fd));
-        break;
+    switch (field.Type) {
       case 'submit':
-        fieldWrapper.append(createButton(fd));
+        fieldWrapper.append(createButton(field));
         break;
       default:
-        // For now, we are not creating any label for the email field
-        //   fieldWrapper.append(createLabel(fd));
-        fieldWrapper.append(createInput(fd));
-    }
-
-    if (fd.Rules) {
-      try {
-        rules.push({ fieldId, rule: JSON.parse(fd.Rules) });
-      } catch (e) {
-        // eslint-disable-next-line no-console
-        console.warn(`Invalid Rule ${fd.Rules}: ${e}`);
-      }
+        fieldWrapper.append(createInput(field));
     }
     form.append(fieldWrapper);
   });
 
-  form.addEventListener('change', () => applyRules(form, rules));
-  applyRules(form, rules);
   fill(form);
   return form;
 }
