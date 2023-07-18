7ece467b7855bc356c81f19b90778fb851075e9e
Sweta Barman <swetabarman@gmail.com>
2023-07-18 14:24:41 -0400

    Remove loading of the footer block

--- scripts/lib-franklin.js
+++ scripts/lib-franklin.js
@@ -588,18 +588,6 @@ export function loadHeader(header) {
 }
 
 /**
- * Loads a block named 'footer' into footer
- * @param footer footer element
- * @returns {Promise}
- */
-export function loadFooter(footer) {
-  const footerBlock = buildBlock('footer', '');
-  footer.append(footerBlock);
-  decorateBlock(footerBlock);
-  return loadBlock(footerBlock);
-}
-
-/**
  * Setup block utils.
  */
 export function setup() {
